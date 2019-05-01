b33b9e774ddf8085c2c7c0c7771221a3be2a7972
August Alfredsson <august.alfredsson@jayway.com>
2019-05-01 16:20:43 +0200

    Hide rest of items if one is clicked

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,55 +1,39 @@
-import React from "react";
-import styled from "styled-components";
-
-const GridContainer = styled.div`
-  width: 100%;
-  max-width: 500px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
-  grid-template-rows: auto auto;
-  grid-gap: 16px;
-`;
-
-const GridItem = styled.div`
-  height: 150px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ProjectCard = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  text-align: center;
-  color: white;
-  border-radius: 5px;
-  transition: box-shadow 0.4s, width 0.4s;
-  box-shadow: 0px 6px 20px 0px #0000002e;
-  background-image: url(${props => props.background});
-  background-size: auto 100%;
-  &:hover {
-    box-shadow: 0px 6px 10px 0px #0000002e;
-  }
-  span {
-    text-shadow: 0px 0px 5px black;
-  }
-`;
+import React, { useState } from "react";
+import { GridContainer, GridItem, ProjectCard } from "./styles.js";
 
 const Grid = ({ list }) => {
+  const [itemIdClicked, setItemIdClicked] = useState(null);
+  console.log(itemIdClicked);
   return (
     <GridContainer>
       {list.map(item => {
-        return (
-          <GridItem>
-            <ProjectCard background={item.image}>
-              <span>{item.name}</span>
-            </ProjectCard>
-          </GridItem>
-        );
+        if (itemIdClicked && itemIdClicked === item.id) {
+          return (
+            <GridItem key={item.id}>
+              <ProjectCard
+                background={item.image}
+                onClick={() => {
+                  setItemIdClicked(item.id);
+                }}
+              >
+                <span>{item.name}</span>
+              </ProjectCard>
+            </GridItem>
+          );
+        } else if (!itemIdClicked) {
+          return (
+            <GridItem key={item.id}>
+              <ProjectCard
+                background={item.image}
+                onClick={() => {
+                  setItemIdClicked(item.id);
+                }}
+              >
+                <span>{item.name}</span>
+              </ProjectCard>
+            </GridItem>
+          );
+        }
       })}
     </GridContainer>
   );
---
+++ src/components/Grid/styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const GridContainer = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  grid-auto-rows: minmax(150px, auto);
+  grid-gap: 16px;
+`;
+
+export const GridItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProjectCard = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  text-align: center;
+  color: white;
+  border-radius: 5px;
+  transition: box-shadow 0.4s, width 0.4s;
+  box-shadow: 0px 6px 20px 0px #0000002e;
+  background-image: url(${props => props.background});
+  background-size: cover;
+  background-position: center center;
+  &:hover {
+    box-shadow: 0px 6px 10px 0px #0000002e;
+  }
+  span {
+    text-shadow: 0px 0px 5px black;
+  }
+`;
